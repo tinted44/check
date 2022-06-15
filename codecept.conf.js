fa8da8625d20f84cede8d72b99e1ad2c6c90e4b6
samuel <adegborosamuel@gmail.com>
2022-06-15 11:02:17 +0100

    check

---
+++ codecept.conf.js
@@ -0,0 +1,17 @@
+exports.config = {
+  tests: 'gig/*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'http://localhost',
+      show: true,
+      browser: 'chromium'
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'check'
+}
---
+++ gig/_test.js
@@ -0,0 +1,7 @@
+Feature('gig');
+
+Scenario('test something', ({ I }) => {
+   I.amOnPage('https://automationparctice.com/index.php')
+   I.wait(4)
+
+});
